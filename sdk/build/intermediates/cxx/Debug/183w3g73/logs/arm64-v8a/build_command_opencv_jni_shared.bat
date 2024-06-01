bdf3645bc951632d3ca59d116e4dbed7130477ea
BotShipper <Quanaptx4869@gmail.com>
2024-06-01 20:45:04 +0700

    add demo

--- sdk/build/intermediates/cxx/Debug/183w3g73/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ sdk/build/intermediates/cxx/Debug/183w3g73/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
 "D:\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "E:\\Projects Android\\NCKH-master 2\\sdk\\.cxx\\Debug\\183w3g73\\arm64-v8a" ^
+  "E:\\Projects Android\\NCKH-master\\sdk\\.cxx\\Debug\\183w3g73\\arm64-v8a" ^
   opencv_jni_shared
